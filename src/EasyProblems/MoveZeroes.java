b23518ed25b5c4f0ee5b1378819eacb9105fde5d
Mehmet Gezer <gezermehmet324@gmail.com>
2025-12-09 00:45:26 +0300

    Update Move Zeroes solve

--- src/EasyProblems/MoveZeroes.java
+++ src/EasyProblems/MoveZeroes.java
@@ -1,4 +1,24 @@
 package EasyProblems;
 
+import java.util.Arrays;
+
 public class MoveZeroes {
+
+
+    static void main() {
+        int[] nums = {0, 1, 2, 0, 0, 0, 3, 9, 12};
+
+        int zero = 0;
+        int i = 0;
+
+        for (; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                int temp = nums[zero];
+                nums[zero] = nums[i];
+                nums[i] = temp;
+                zero++;
+            }
+        }
+        System.out.println(Arrays.toString(nums));
+    }
 }
